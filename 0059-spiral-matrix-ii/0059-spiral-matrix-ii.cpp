f93f7b981bc3d8e6195adac3da2104642615db5a
Inam ul Rehman <113470083+inamprograms@users.noreply.github.com>
2023-12-30 20:04:58 +0500

    Time: 5 ms (36.81%), Space: 7.2 MB (7.65%) - LeetHub

--- 0059-spiral-matrix-ii/0059-spiral-matrix-ii.cpp
+++ 0059-spiral-matrix-ii/0059-spiral-matrix-ii.cpp
@@ -3,46 +3,36 @@ public:
     vector<vector<int>> generateMatrix(int n) {
         
        vector<vector<int>> matrix(n, vector<int>(n, 0));
-    //     for (int i = 0; i < n; ++i) {
-    //     for (int j = 0; j < n; ++j) {
-    //         std::cout << matrix[i][j] << " ";
-    //     }
-    //     std::cout << std::endl;
-    // }
+   
         int start = 1;
-        int left = 0, right = n - 1; 
-        int top = 0, buttom = n - 1;
+        int left = 0, right = n; 
+        int top = 0, buttom = n;
         
-        while (left <= right) {
+          while (left < right || top < buttom) {
             
-            // fill from left to right/ top row
-            for (int c = left; c <= right; c++) {
-                matrix[top][c] = start;
-                start++;
-            }
-            top++;
+                for (int i = left; i < right; i++) {
+                    matrix[top][i] = start;
+                    start++;
+                }
+                top++;
+                
+                for (int i = top; i < buttom; i++) {
+                    matrix[i][right - 1] = start;
+                    start++;
+                }
+                right--;
+                
+                for (int i = right - 1; i >= left; i--) { 
+                    matrix[buttom - 1][i] = start;
+                    start++;
+                }
+                buttom--;
             
-            // fill from top to buttom / right col
-            for (int r = top; r <= buttom; r++) {
-                matrix[r][right] = start;
-                start++;
-            }
-            right--;
-            
-            // fill from right to left / buttom row
-            for (int c = right; c >= left; c--) {
-                matrix[buttom][c] = start;
-                start++;
-            }
-            buttom--;
-            
-            // fill from buttom to top / left col
-            for (int r = buttom; r >= top; r--) {
-                matrix[r][left] = start;
-                start++;
-            }
-            left++;
-
+                for (int i = buttom - 1; i >= top; i--) {
+                    matrix[i][left] = start;
+                    start++;
+                }
+                left++;
         }
         return matrix;
     }
