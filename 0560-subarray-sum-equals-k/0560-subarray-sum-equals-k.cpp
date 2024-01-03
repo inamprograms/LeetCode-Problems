66d68ce34c6956e0551e1e6aeb0b50f140ce504c
Inam ul Rehman <113470083+inamprograms@users.noreply.github.com>
2024-01-03 23:36:32 +0500

    Time: 62 ms (88.63%), Space: 42 MB (58.34%) - LeetHub

---
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int subarraySum(vector<int>& nums, int k) {
+        
+       int count = 0, curSum = 0;
+        unordered_map<int, int> prefixSum;
+        prefixSum[0] = 1;
+        
+        for (int i = 0; i < nums.size(); i++) {
+            
+            curSum += nums[i];
+            
+            int diff = curSum - k;
+            if (prefixSum.find(diff) != prefixSum.end()) {
+                count += prefixSum[diff];
+            }
+            
+            prefixSum[curSum]++;
+            
+            
+        }
+        
+        return count;
+    }
+};
