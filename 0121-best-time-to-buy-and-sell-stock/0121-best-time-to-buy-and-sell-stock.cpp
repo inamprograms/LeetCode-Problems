c700c0cd2c62ab021d8c7d1673239771bb814cd6
Inam ul Rehman <113470083+inamprograms@users.noreply.github.com>
2024-01-07 18:11:10 +0500

    Time: 85 ms (92.13%), Space: 93.6 MB (58.91%) - LeetHub

---
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        
+        int profit = 0;
+        
+        int minPrice = INT_MAX;
+        
+        for (int i = 0; i < prices.size(); i++) {
+            
+            if (prices[i] < minPrice) {
+                
+                minPrice = prices[i];
+            } else {
+                
+                if (prices[i] - minPrice > profit) {
+                    profit = prices[i] - minPrice;
+                }
+            }
+           
+        }
+        
+        return profit;
+    }
+    
+};
