ee6098beb8d7c908f39b4bec0ace1940d1dd145f
Inam ul Rehman <113470083+inamprograms@users.noreply.github.com>
2024-01-09 19:18:19 +0500

    Time: 96 ms (39.01%), Space: 26.4 MB (45.67%) - LeetHub

--- 2390-removing-stars-from-a-string/2390-removing-stars-from-a-string.cpp
+++ 2390-removing-stars-from-a-string/2390-removing-stars-from-a-string.cpp
@@ -1,23 +1,20 @@
 class Solution {
 public:
     string removeStars(string s) {
-        stack<char> stk;
-        
-        for (char ch: s) {
-            if (ch == '*') {
-                stk.pop();
-            } else {
-                stk.push(ch);
-            }
-        }
         string res = "";
-        while (!stk.empty()) {
-            char ch = stk.top();
-            // res =  ch + res;
-            res += ch;
-            stk.pop();
+        
+        int star = 0;
+        
+        while (star < s.size()) {
+            
+            if (s[star] == '*') {
+                res.pop_back();
+            } else {
+                res += s[star];
+            }
+            star++;
+            
         }
-        reverse(res.begin(), res.end());
         return res;
     }
 };
