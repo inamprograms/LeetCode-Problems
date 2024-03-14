fc2918222fc6550fd5865f4b40e052591cd1512d
Inam ul Rehman <113470083+inamprograms@users.noreply.github.com>
2024-03-15 01:26:01 +0500

    Time: 0 ms (100.00%), Space: 7.7 MB (5.47%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.cpp
+++ 0007-reverse-integer/0007-reverse-integer.cpp
@@ -1,9 +1,11 @@
 class Solution {
 public:
-    long int num = 0;
+
     int reverse(int x) {
-        
-        rev(x);
+        if (x == 0) {
+            return 0;
+        }
+        long int num = rev(x);
         
         if(num < pow(-2, 31) || num > pow(2, 31)-1)
             return 0;
@@ -11,14 +13,32 @@ public:
         return num;
     }
     
-    void rev(int n) {
+    long int rev(int n) {
+        int digits;
+        if (n < 0) {
+            long int temp = n;
+            temp = -1 * temp;
+            digits = log10(temp) + 1;
+        } else {
+            digits = log10(n) + 1;
+        }
+        
+        // int digits =  digits = log10(n) + 1;
+       
+       return helper(n, digits);
+    }
+    
+    long int helper(int n, int digits) {
+        // if n is one digit number
+        // if(n%10 == 0) {
+        //     return n;
+        // }
         
         if (n == 0) {
-            return;
+            return 0;
         }
-        long int rem = n % 10;
-        num = num * 10 + rem;
         
-        rev(n / 10);
+        int rem = n % 10;
+        return rem * pow(10, digits-1) + helper(n / 10, digits-1);
     }
 };
