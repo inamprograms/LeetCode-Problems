238428a7a644fe296c775a1eaf35d20c431faf22
Inam ul Rehman <113470083+inamprograms@users.noreply.github.com>
2025-01-21 12:36:34 +0500

    Time: 201 ms (5.43%), Space: 62.8 MB (100%) - LeetHub

---
+++ 2145-grid-game/2145-grid-game.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    long long gridGame(vector<vector<int>>& grid) {
+        int n = grid[0].size();
+
+        long long top = grid[0][0], buttom = 0, secondRobo, res = LONG_MAX;
+
+        for(int i = 1; i < grid[0].size(); i++){
+            top += grid[0][i];
+        } 
+
+        for(int i = 0; i < grid[0].size(); i++){
+            top -= grid[0][i];
+            secondRobo = max(top, buttom);
+            res = min(res, secondRobo);
+            buttom += grid[1][i];
+        }
+    return res;
+    }
+};
+
