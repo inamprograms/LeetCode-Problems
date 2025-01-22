a125a8aa8cd774728897441d48252440c4f8edd4
Inam ul Rehman <113470083+inamprograms@users.noreply.github.com>
2025-01-22 20:48:38 +0500

    Time: 0 ms (100%), Space: 18.2 MB (56.06%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.cpp
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -4,12 +4,10 @@ public:
         int rows = image.size(), cols = image[0].size();
         int dx[4] = {0, 0, 1, -1}, dy[4] = {1, -1, 0, 0};
         queue<pair<int, int>> q;
-        set<pair<int, int>> seen;
         int spc = image[sr][sc];
 
         q.push({sr, sc});
-        seen.insert({sr, sc});
-        
+       
         while(!q.empty()) { 
             auto [cr, cc] = q.front();
             q.pop();
@@ -18,11 +16,10 @@ public:
             for (int d = 0; d < 4; d++) {
                 int nr =  dx[d] + cr, nc = dy[d] + cc;
                 if (nr < 0 || nr >= rows || nc < 0 || nc >= cols) continue;
-                if (seen.find({nr, nc}) != seen.end()) continue;
+                if (image[nr][nc] == color) continue;
                 if (image[nr][nc] != spc) continue;
 
                 q.push({nr, nc});
-                seen.insert({nr, nc});
             }
         }
         return image;
