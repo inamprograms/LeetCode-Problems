5883194985a219941026dc2d6b1f549ee8727c6b
Inam ul Rehman <113470083+inamprograms@users.noreply.github.com>
2025-01-22 15:12:18 +0500

    Time: 81 ms (6.59%), Space: 117.3 MB (5.03%) - LeetHub

--- 0463-island-perimeter/0463-island-perimeter.cpp
+++ 0463-island-perimeter/0463-island-perimeter.cpp
@@ -2,27 +2,46 @@ class Solution {
 public:
 
     int islandPerimeter(vector<vector<int>>& grid) {
+        int side = 0;
+        bool found = false;
         int rows = grid.size();
         int cols = grid[0].size();
-        set<pair<int,int>> landRegion;
+        queue<pair<int, int>> q;
+        set<pair<int, int>> seen;
+
+        int dx[4] = {0, 0, 1, -1};
+        int dy[4] = {1, -1, 0, 0};
+
         for (int r = 0; r < rows; r++) {
             for (int c = 0; c < cols; c++) {
-                if (grid[r][c] == 1) landRegion.insert({r,c});
-            }
-        }
-
-        int output = 0;
-        for (auto land : landRegion) {
-            int r = land.first, c = land.second;
-            output += 4;
-            vector<pair<int, int>> neighbors = {{r - 1, c}, {r + 1, c}, {r, c - 1}, {r, c + 1}};
-            for (auto neighbor : neighbors) {
-                if (landRegion.find(neighbor) != landRegion.end()) {
-                    output -= 1;
+                if (grid[r][c] == 1) {
+                    q.push({r, c});
+                    seen.insert({r, c});
+                    found = true;
+                    break;
                 }
             }
-    }
-    return output;
-      
+            if (found) break;
+        }
+
+        while(!q.empty()) {
+           
+            pair<int, int> land = q.front();
+            q.pop();
+
+            for (int d = 0; d < 4; d++) {
+                int nr = land.first + dx[d], nc = land.second + dy[d];
+                if (nr < 0 || nr >= rows || nc < 0 || nc >= cols || grid[nr][nc] == 0) {
+                    side++;
+                    continue;
+                }
+
+                if (seen.find({nr, nc}) != seen.end()) continue;
+                q.push({nr, nc});
+                seen.insert({nr, nc});
+
+            }
+        }
+        return side;
     }
 };
