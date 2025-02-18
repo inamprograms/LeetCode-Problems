15801bec9c108a67456dd2e45aa3dce3fa690548
Inam ul Rehman <113470083+inamprograms@users.noreply.github.com>
2025-02-18 11:54:40 +0500

    Time: 11 ms (30.74%), Space: 127.7 MB (88.05%) - LeetHub

--- 2265-partition-array-according-to-given-pivot/2265-partition-array-according-to-given-pivot.cpp
+++ 2265-partition-array-according-to-given-pivot/2265-partition-array-according-to-given-pivot.cpp
@@ -3,24 +3,30 @@ public:
     vector<int> pivotArray(vector<int>& nums, int pivot) {
         int n = nums.size();
         vector<int> result(n, 0);
-        vector<int> middle;
-        vector<int> larger;
-        int it = 0;
-        for (int i = 0; i < n; i++) {
-            if (nums[i] < pivot) {
-                result[it] = nums[i];
-                it++;
-            } else if (nums[i] > pivot) larger.push_back(nums[i]);
-            else middle.push_back(nums[i]);
-        }
+        int smaller = 0, larger = 0;
 
-        for (int i = 0; i < middle.size(); i++) {
-            result[it] = middle[i];
-            it++;
+        for (int i = 0; i < n; i++) {
+            if (nums[i] < pivot) smaller++;
+            else if (nums[i] > pivot) larger++;
+
         }
-        for (int i = 0; i < larger.size(); i++) {
-            result[it] = larger[i];
-            it++;
+        int m_it = smaller, l_it = smaller + (n - smaller - larger), s_it = 0;
+    
+        for (int i = 0; i < n; i++) {
+    
+            if (nums[i] == pivot) {
+                result[m_it] = nums[i];
+                m_it++;
+            } 
+            if (nums[i] > pivot) {
+                result[l_it] = nums[i];
+                l_it++;
+            }
+            if (nums[i] < pivot) {
+                result[s_it] = nums[i];
+                s_it++;
+            }
+      
         }
         return result;
     }
