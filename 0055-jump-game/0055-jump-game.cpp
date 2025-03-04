4aff2d601a19fb3aae31607f07b122212f31b06f
Inam ul Rehman <113470083+inamprograms@users.noreply.github.com>
2025-03-04 15:56:20 +0500

    Time: 0 ms (100%), Space: 52.2 MB (85.82%) - LeetHub

--- 0055-jump-game/0055-jump-game.cpp
+++ 0055-jump-game/0055-jump-game.cpp
@@ -1,15 +1,11 @@
 class Solution {
 public:
     bool canJump(vector<int>& nums) {
-        
-        int goal = nums.size() - 1;
-        for (int i = nums.size() - 1; i >= 0; i--) {
-            
-            if (i + nums[i] >= goal) { 
-                goal = i;
-            }
+        int n = nums.size();
+        int goal = n - 1;
+        for (int cur_pos = n - 2; cur_pos >= 0; cur_pos--) {
+            if (cur_pos + nums[cur_pos] >= goal) goal = cur_pos;    
         }
-        
         return goal == 0;
     }
 };
