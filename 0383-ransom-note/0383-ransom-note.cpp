18797ddfc852e64e41ad58eb040e0065e2e9b278
Inam ul Rehman <113470083+inamprograms@users.noreply.github.com>
2025-03-27 12:20:43 +0500

    Time: 2 ms (67.95%), Space: 12 MB (29.69%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.cpp
+++ 0383-ransom-note/0383-ransom-note.cpp
@@ -2,21 +2,17 @@ class Solution {
 public:
     bool canConstruct(string ransomNote, string magazine) {
         unordered_map<char, int> map;
-
-        for (int i = 0; i < magazine.size(); i++) {
-            char ch = magazine[i];
-
+        for (char ch: magazine) {
             map[ch]++;
         }
 
-        for (int i = 0; i < ransomNote.size(); i++) {
-            char ch = ransomNote[i];
-
-            if (map[ch] == 0) {
+        for (char ch: ransomNote) {
+            if (map.find(ch) == map.end() || map[ch] <= 0) {
                 return false;
             }
             map[ch]--;
         }
+
 
         return true;
     }
