5b3e0c794528ace8a298e4c43903116b59fd3691
Inam ul Rehman <113470083+inamprograms@users.noreply.github.com>
2025-03-29 13:44:56 +0500

    Time: 51 ms (82.27%), Space: 81.3 MB (27.48%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.cpp
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.cpp
@@ -1,23 +1,15 @@
 class Solution {
 public:
     bool containsNearbyDuplicate(vector<int>& nums, int k) {
-        set<int> window;
-        
-        int l = 0;
-        for (int r = 0; r < nums.size(); r++) {
-            
-            if (r - l > k) {
-                window.erase(nums[l]);
-                l++;
-            }
-            
-            if (window.find(nums[r]) != window.end()) {
+        unordered_map<int, int> map;
+        for (int i = 0; i < nums.size(); i++) {
+            if (map.find(nums[i]) != map.end() && abs(map[nums[i]] - i) <= k) {
                 return true;
             }
-            
-            window.insert(nums[r]);
+
+            map[nums[i]] = i;
         }
-        
+
         return false;
     }
 };
