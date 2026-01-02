3922bf162b8ecda83fe11ac89e5f0006c5aa4d04
Inam ul Rehman <113470083+inamprograms@users.noreply.github.com>
2026-01-02 08:07:04 +0500

    Time: 0 ms (100%), Space: 8.3 MB (11.93%) - LeetHub

--- 0191-number-of-1-bits/0191-number-of-1-bits.cpp
+++ 0191-number-of-1-bits/0191-number-of-1-bits.cpp
@@ -1,14 +1,12 @@
 class Solution {
 public:
-    int hammingWeight(uint32_t n) {
+    int hammingWeight(int n) {
+        int count = 0;
+        while (n > 0) {
+            if ((n & 1) != 0) count++;
+            n = n >> 1;
+        }
 
-    int count = 0;
-    while (n)
-    {
-        n = n & (n - 1);
-        count++;
-    }
-    return count;
-
+        return count;
     }
 };
