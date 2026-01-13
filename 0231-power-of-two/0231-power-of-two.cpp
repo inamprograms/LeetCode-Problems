ae3957f456f01a6bfbb869d382cbcd0760118d8b
Inam ul Rehman <113470083+inamprograms@users.noreply.github.com>
2026-01-13 19:27:48 +0500

    Time: 0 ms (100%), Space: 7.9 MB (13.73%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.cpp
+++ 0231-power-of-two/0231-power-of-two.cpp
@@ -2,13 +2,26 @@ class Solution {
 public:
     bool isPowerOfTwo(int n) {
 
-        // if (n <= 0) return false;
+        if (n <= 0) return false;
+        
         // while (n != 1) {
         //     if ((n & 1 )== 1) return false;
         //     n = n >> 1;
         // }
         // return true;
-        return n > 0 && (n & (n - 1)) == 0;
+
+        // return n > 0 && (n & (n - 1)) == 0;
+
+
+        int count = 0;
+        while (n) {
+            if (count > 0) {
+                return false;
+            }
+            if (n % 2 == 1) count++;
+            n /= 2;
+        }
+        return true;
        
     }
 };
