065e2f46aa67a7f7a785de3238b2f45ac6cc73b9
Inam ul Rehman <113470083+inamprograms@users.noreply.github.com>
2026-01-13 20:36:16 +0500

    Time: 0 ms (100%), Space: 7.8 MB (79.21%) - LeetHub

--- 0342-power-of-four/0342-power-of-four.cpp
+++ 0342-power-of-four/0342-power-of-four.cpp
@@ -1,20 +1,25 @@
 class Solution {
 public:
     bool isPowerOfFour(int n) {
-        // if (n <= 0) return false;
-        // while (n != 1) {
-        //     if ((n & 4 ) > 0) return false;
-
-        //     n = n >> 4;
-
-        // }
-        // return true;
-
         if (n <= 0) return false;
-        int base = 4;
-        double res = log(n) / log(base);
-        if (floor(res) == res) return true;
-        return false;
+
+        while (n > 1) {
+            if ((n & 3) != 0) return false; // not divisible by 4
+            n = n >> 2;  // divide by 4
+        }
+        return true;
+
+    
+        // int base = 4;
+        // double res = log(n) / log(base);
+        // if (floor(res) == res) return true;
+        // return false;
+
+        // return (n & (n - 1)) == 0 && (n % 3) == 1;
+
+        if ((n & (n - 1)) != 0) return false;
+        // the single 1 bit must be at even position
+        return (n & 0x55555555) != 0;
     }
     
     
