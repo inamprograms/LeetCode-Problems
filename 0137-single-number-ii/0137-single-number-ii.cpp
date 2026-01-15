346bf704d3f3b63bf8dae241f31adfe050d3cb74
Inam ul Rehman <113470083+inamprograms@users.noreply.github.com>
2026-01-15 10:34:44 +0500

    Time: 4 ms (14.17%), Space: 13.2 MB (98.26%) - LeetHub

---
+++ 0137-single-number-ii/0137-single-number-ii.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int res = 0;
+
+        for (int i = 0; i < 32; i++) {
+
+            int count = 0;
+            for (int num: nums) {
+
+                if (((num >> i) & 1) == 1) {
+                    count++;
+                }
+            }
+
+            if (count % 3 != 0) {
+                res = res | (1 << i);
+            } 
+        }
+
+        return res;
+    }
+};
