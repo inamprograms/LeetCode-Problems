d3c1a3cf0ea4fcb9dec071a3f5d83a23a91d1a16
Inam ul Rehman <113470083+inamprograms@users.noreply.github.com>
2026-01-25 20:00:07 +0500

    Time: 74 ms (5.01%), Space: 113.2 MB (5.65%) - LeetHub

---
+++ 1310-xor-queries-of-a-subarray/1310-xor-queries-of-a-subarray.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    vector<int> xorQueries(vector<int>& arr, vector<vector<int>>& queries) {
+        vector<int> res;
+        unordered_map<int, int> map;
+
+        int val = 0;
+        for (int key = 0; key < arr.size(); key++) {
+            val ^= arr[key];
+            map[key] = val;
+        }
+
+        for (vector<int> query: queries) {
+            int start = query[0]; 
+            int end = query[1];
+
+            if (start > 0) {
+                res.push_back(map[start - 1] ^ map[end]);
+            } else {
+                res.push_back(map[end]);
+            }
+        }
+
+
+        // Brute Force
+        // for (vector<int> query: queries) {
+        //     int start = query[0];
+        //     int end = query[1];
+        //     int ans = 0;
+        //     for (int i = start; i <= end; i++) {
+        //         ans ^= arr[i];
+        //     }
+        //     res.push_back(ans);
+        // }
+        return res;
+
+
+    }
+};
