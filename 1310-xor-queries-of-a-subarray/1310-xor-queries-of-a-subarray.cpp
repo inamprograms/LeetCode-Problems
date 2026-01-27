3a1eca13355b0d2f93044839c7ebf7e54a57a859
Inam ul Rehman <113470083+inamprograms@users.noreply.github.com>
2026-01-27 14:45:18 +0500

    Time: 32 ms (20.71%), Space: 100.5 MB (18.26%) - LeetHub

--- 1310-xor-queries-of-a-subarray/1310-xor-queries-of-a-subarray.cpp
+++ 1310-xor-queries-of-a-subarray/1310-xor-queries-of-a-subarray.cpp
@@ -2,24 +2,35 @@ class Solution {
 public:
     vector<int> xorQueries(vector<int>& arr, vector<vector<int>>& queries) {
         vector<int> res;
-        unordered_map<int, int> map;
-
-        int val = 0;
-        for (int key = 0; key < arr.size(); key++) {
-            val ^= arr[key];
-            map[key] = val;
+        
+        int n = arr.size();
+        vector<int> prefix((n + 1), 0);
+        for (int i = 0; i < n; i++)  {
+            prefix[i+1] = arr[i] ^ prefix[i];
         }
-
         for (vector<int> query: queries) {
-            int start = query[0]; 
-            int end = query[1];
-
-            if (start > 0) {
-                res.push_back(map[start - 1] ^ map[end]);
-            } else {
-                res.push_back(map[end]);
-            }
+            int l  = query[0];
+            int r  = query[1] + 1;
+            res.push_back(prefix[r] ^ prefix[l]);
         }
+
+        // unordered_map<int, int> map;
+        // int val = 0;
+        // for (int key = 0; key < arr.size(); key++) {
+        //     val ^= arr[key];
+        //     map[key] = val;
+        // }
+
+        // for (vector<int> query: queries) {
+        //     int start = query[0]; 
+        //     int end = query[1];
+
+        //     if (start > 0) {
+        //         res.push_back(map[start - 1] ^ map[end]);
+        //     } else {
+        //         res.push_back(map[end]);
+        //     }
+        // }
 
 
         // Brute Force
