5080570729f67004b7dc362ca4f3f1b3f64f126e
Inam ul Rehman <113470083+inamprograms@users.noreply.github.com>
2026-02-06 23:05:12 +0500

    Time: 0 ms (100%), Space: 19.5 MB (12.35%) - LeetHub

--- 3370-smallest-number-with-all-set-bits/3370-smallest-number-with-all-set-bits.py
+++ 3370-smallest-number-with-all-set-bits/3370-smallest-number-with-all-set-bits.py
@@ -1,9 +1,16 @@
 class Solution:
     def smallestNumber(self, n: int) -> int:
-        res = 0
-        for i in range(len(bin(1000))+ 1):
+        # res = 0
+        # for i in range(len(bin(1000))+ 1):
+        #     if n & (1 << 12 - i):
+        #         for j in range(12-i + 1):
+        #             res = res | 1 << j
+        #         break
+        # return res
+
+        for i in range(len(bin(1000)) + 1):
             if n & (1 << 12 - i):
-                for j in range(12-i + 1):
-                    res = res | 1 << j
-                break
-        return res
+                return 2** (12-i+1) - 1
+        
+        bits = n.bit_length()
+        return (1 << bits) - 1
