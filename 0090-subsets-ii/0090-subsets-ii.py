0fd94b3e97f9d9de31a413af43752340859b41c7
Inam ul Rehman <113470083+inamprograms@users.noreply.github.com>
2026-02-08 15:24:37 +0500

    Time: 3 ms (23.02%), Space: 19.5 MB (35.12%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.py
+++ 0090-subsets-ii/0090-subsets-ii.py
@@ -1,14 +1,36 @@
 class Solution:
     def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
-        res = [[]]
-        freq = {}
+        # res = [[]]
+        # freq = {}
+        # # nums.sort()
+        # for num in nums:
+        #     subsets = []
+        #     for cur in res:
+        #         key = tuple(cur + [num])
+        #         if key not in freq:
+        #             subsets.append(list(key))
+        #             freq[key] = 1
+        #     res += subsets
+        # return res
+
+
+        res = []
+        subset = []
         nums.sort()
-        for num in nums:
-            subsets = []
-            for cur in res:
-                key = tuple(cur + [num])
-                if key not in freq:
-                    subsets.append(list(key))
-                    freq[key] = 1
-            res += subsets
-        return res
+        
+        def create_subset(i):
+            if i == len(nums):
+                res.append(subset[:])
+                return
+            
+            subset.append(nums[i])
+            create_subset(i+1)
+            subset.pop()
+            
+            while i + 1 < len(nums) and nums[i] == nums[i+1]:
+                i += 1
+                
+            create_subset(i+1)
+        
+        create_subset(0)
+        return res      
