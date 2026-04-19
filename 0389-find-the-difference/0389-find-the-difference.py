30f5108aae92b8dc8467c51b6f6844ed19cf98b1
Inam ul Rehman <113470083+inamprograms@users.noreply.github.com>
2026-04-19 12:28:35 +0500

    Time: 0 ms (100%), Space: 19.3 MB (32.57%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.py
+++ 0389-find-the-difference/0389-find-the-difference.py
@@ -1,15 +1,21 @@
 class Solution:
     def findTheDifference(self, s: str, t: str) -> str:
-        dic = {}
-        res = ""
-        for ch in s:
-            # if ch not in dic:
-            #     dic[ch] = 0
-            # dic[ch] += 1
-            dic[ch] = dic.get(ch, 0) + 1
+        # dic = {}
+        # res = ""
+        # for ch in s:
+        #     # if ch not in dic:
+        #     #     dic[ch] = 0
+        #     # dic[ch] += 1
+        #     dic[ch] = dic.get(ch, 0) + 1
         
-        for ch in t:
-            if ch not in dic or dic[ch] == 0:
-                return ch
-            dic[ch] -= 1
+        # for ch in t:
+        #     if ch not in dic or dic[ch] == 0:
+        #         return ch
+        #     dic[ch] -= 1
+
+        s = s + t
+        res = 0
+        for ch in s:
+            res ^= ord(ch)
+        return chr(res)
     
